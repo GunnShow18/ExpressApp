e0064c4db8630624d49590a84dd0d3b732492dc8
Harland Gunn <HGunn@harlands-mbp.attlocal.net>
2018-12-07 20:51:29 -0500

    second commit

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require('express');
 const app = express();
 // sets path
 const path = require('path');
-// call Pug interpretation into code
+// bringing privacy function into file
+require('dotenv').load();
 
+// call Pug interpretation into code
 app.set('view engine', 'pug');
+
 //This will make Express look for your templates in the "views" directory, 
 //relative to the file in which you specified the above line.
 
@@ -14,25 +17,31 @@ app.set('views', path.join(__dirname, 'views'));
 //folders must be views and scripts or else pug wont work
 // pug is baby view of react, so much laws that govern pug will passover to react
 
+
 // creates portal to Home Page
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname+'/views/index.html'));
 //     app.use(express.static(__dirname + '/public'));  
 // });
 
+// calling in public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// // calling public SCRIPTS into express
+// app.use(express.static(path.join(__dirname, '/public/javascripts')));
+
+// calling bootstrap styling into application
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
+
+// calling css styling into application
+app.use('/stylesheets', express.static(__dirname + '/public/stylesheets'));
+
+// calling includes folder into pug layout template
+app.use('/includes', express.static(__dirname + '/views/includes'));
+
 // // creates portal to "pug version" of Home Page
 app.get("/", (req, res) => {
     res.render('maps', { title: 'hey', message: 'hello there' })
-});
-
-// creates portal to youTube URL Page
-app.get('/url', (req, res) => {
-    res.render('url', { title: 'hey', message: 'hello there' })
-});
-
-// creates portal to youTube shortened URL Page
-app.get('/urlComp', (req, res) => {
-    res.render('urlComp', { title: 'hey', message: 'hello there' })
 });
 
 // creates portal to youTube page
@@ -40,11 +49,15 @@ app.get('/youTube', (req, res) => {
     res.render('youtube', { title: 'hey', message: 'hello there' })
 });
 
-// creates portal to youTube top 10 page
-app.get('/youTube10', (req, res) => {
-    res.render('youtubeComp', { title: 'hey', message: 'hello there' })
+// creates portal to youTube page
+app.get('/', (req, res) => {
+    res.render('maps', {KEY: process.env.KEY})
 });
 
+// creates portal to youTube page
+app.get('/youtube', (req, res) => {
+    res.render('youtube', {KEY: process.env.KEY})
+});
 
 app.listen(3000);
 
---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+// maps init Function
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat:33.7490 , lng:-84.3880},
+        zoom: 12,
+    })
+}
+
